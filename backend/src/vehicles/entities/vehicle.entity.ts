424174c44fcd36f4986fcbf83a197d56abfc5227
diegomaustem <diego.maustem@gmail.com>
2025-10-25 15:25:25 -0300

    :sparkles: feat: Criação entidade veiculos

---
+++ backend/src/vehicles/entities/vehicle.entity.ts
@@ -0,0 +1,11 @@
+export class Vehicle {
+  id: string;
+  placa: string;
+  chassi: string;
+  renavam: string;
+  modelo?: string;
+  marca: string;
+  ano: number;
+  criadoEm: Date;
+  atualizadoEm: Date;
+}
